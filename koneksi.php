860c4e1ae3514af7d0a9360916e41b03929ca778
KoplarKoprol <putu.moreno298@gmail.com>
2024-01-23 08:42:14 +0800

    add : Koneksi MySQL

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Galeri20240115";
+
+// Create connection
+$konek = mysqli_connect($servername, $username, $password, $database);
+?>
