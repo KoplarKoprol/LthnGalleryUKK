45e8326e4cecc20acbdff239ba7ea3865a940f81
KoplarKoprol <putu.moreno298@gmail.com>
2024-01-27 11:08:39 +0800

    add : phpingfo.php & check connection
    check connection added on koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -5,5 +5,13 @@
 // $database = "Galeri20240115";
 
 // Create connection
-$conn=mysqli_connect("localhost","root","","Galeri2024010");
+$conn=mysqli_connect("localhost","root","","Galeri20240115");
+
+// Check koneksi
+
+// if (!$conn) {
+//     die("Koneksi gagal: " . mysqli_connect_error());
+// }
+// echo "Koneksi berhasil";
+// mysqli_close($conn);
 ?>
