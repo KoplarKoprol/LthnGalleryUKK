806639c6d3feed50232ffa76274faa50c1fd6798
KoplarKoprol <putu.moreno298@gmail.com>
2024-01-27 11:07:05 +0800

    fix : register
    ternyata klo mau liat errornya harus manggil command dulu

--- proses_register.php
+++ proses_register.php
@@ -1,13 +1,18 @@
 <?php 
+    // Buat liat error
+
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+
     include "koneksi.php";
 
-    $username=$_POST['Username'];
-    $password=$_POST['Password'];
-    $email=$_POST['Email'];
-    $namaLengkap=$_POST['NamaLengkap'];
-    $alamat=$_POST['Alamat'];
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $email=$_POST['email'];
+    $namaLengkap=$_POST['namaLengkap'];
+    $alamat=$_POST['alamat'];
 
-    $sql=mysqli_query($conn,"insert into user values('','$username','$password','$email','$namaLengkap','$alamat')");
+    $sql = mysqli_query($conn, "INSERT INTO user (username, password, email, namaLengkap, alamat) VALUES ('$username', '$password', '$email', '$namaLengkap', '$alamat')");
 
     header("location:login.php");
 ?>
