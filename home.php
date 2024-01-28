f220ece94a9d9e3284e9ec5ebef0b08c40256e79
KoplarKoprol <putu.moreno298@gmail.com>
2024-01-28 19:44:29 +0800

    add home & album

---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+session_start();
+if (!isset($_SESSION['UserID'])) {
+    header("location:login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Beranda</title>
+</head>
+
+<body>
+    <h1>Halaman Beranda</h1>
+    <p>Selamat Datang <b>
+            <?= $_SESSION['NamaLengkap'] ?>
+        </b></p>
+
+    <ul>
+        <li><a href="album.php">Album</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+
+</html>
