b49162350d6c3b09e50527b5121b7dd45a34d36e
KoplarKoprol <putu.moreno298@gmail.com>
2024-02-01 10:45:22 +0800

    add 'unlink' feature
    for also deleting the file saved locally on gambar folder

--- hapus_foto.php
+++ hapus_foto.php
@@ -1,6 +1,5 @@
 <?php
 // Buat liat error
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -9,7 +8,21 @@ session_start();
 
 $FotoID = $_GET['fotoid'];
 
-$sql = mysqli_query($conn, "DELETE FROM foto WHERE FotoID='$FotoID'");
+// Get the file name from the database
+$sql = mysqli_query($conn, "SELECT * FROM foto WHERE FotoID='$FotoID'");
+$data = mysqli_fetch_array($sql);
+$filename = $data['LokasiFile'];
+
+// Delete the database record
+$sql_delete = mysqli_query($conn, "DELETE FROM foto WHERE FotoID='$FotoID'");
+
+// Delete the image file from the folder
+if ($sql_delete) {
+    $file_path = 'gambar/' . $filename;
+    if (file_exists($file_path)) {
+        unlink($file_path); // Delete the file
+    }
+}
 
 header("location:foto.php");
 ?>
